e392daec1b0eaf84851e692affa68f7a8a386823
Your Name <vishnyakov.andrey@svitsoft.com>
2020-10-19 17:52:43 +0300

    DZ-15-1

--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
+
 public class MainPageTest {
     private WebDriver driver;
     private MainPage mainPage;
